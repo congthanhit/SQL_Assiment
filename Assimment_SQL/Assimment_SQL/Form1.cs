78f68a39f9e321441e4f8b905abee7e2b6d63f6d
congthanhit <truongcong367@gmail.com>
2015-04-11 20:55:30 +0700

    Create Login From

--- Assimment_SQL/Assimment_SQL/Form1.cs
+++ Assimment_SQL/Assimment_SQL/Form1.cs
@@ -16,5 +16,25 @@ namespace Assimment_SQL
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
